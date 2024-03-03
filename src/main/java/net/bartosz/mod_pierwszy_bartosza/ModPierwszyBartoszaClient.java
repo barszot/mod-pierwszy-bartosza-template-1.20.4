8ff8897d3f86527dca2a57d1afb39709f3cbd0df
barszot <barszott@gmail.com>
2024-03-03 15:49:27 +0100

    Initial commit

---
+++ src/main/java/net/bartosz/mod_pierwszy_bartosza/ModPierwszyBartoszaClient.java
@@ -0,0 +1,10 @@
+package net.bartosz.mod_pierwszy_bartosza;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModPierwszyBartoszaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/bartosz/mod_pierwszy_bartosza/ModPierwszyBartoszaDataGenerator.java
@@ -0,0 +1,11 @@
+package net.bartosz.mod_pierwszy_bartosza;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ModPierwszyBartoszaDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
